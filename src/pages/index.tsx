803c5fe021899a7c0c343fb766beace8948650b9
arllyepc <arllyepc@gmail.com>
2024-02-15 04:11:09 -0300

    fix: patials to partials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Advantages from "@patials/index/_advantages";
-import FrequestQuestion from "@patials/index/_frequent-question";
-import Presentation from "@patials/index/_presentation";
+import Advantages from "@partials/index/_advantages";
+import FrequestQuestion from "@partials/index/_frequent-question";
+import Presentation from "@partials/index/_presentation";
 import type { GetStaticProps, NextPage } from "next";
 
 export const getStaticProps: GetStaticProps = async () => {
