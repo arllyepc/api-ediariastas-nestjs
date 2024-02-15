ee6c723e1425bf792c331858bd535a96e3bc0a51
arllyepc <arllyepc@gmail.com>
2024-02-15 04:09:22 -0300

    feat: componentes e stories: pagetitle, safeenvironment

---
+++ src/UI/components/data-display/PageTitle/PageTitle.tsx
@@ -0,0 +1,23 @@
+import React, { PropsWithChildren } from "react";
+
+import {
+  PageTitleContainer,
+  PageTitleStyled,
+  PageSubtitleStyled,
+} from "./PageTitle.styled";
+
+export interface PageTitleProps {
+  title: string;
+  subtitle?: string;
+}
+
+const PageTitle: React.FC<PageTitleProps> = ({ title, subtitle }) => {
+  return (
+    <PageTitleContainer>
+      <PageTitleStyled>{title}</PageTitleStyled>
+      <PageSubtitleStyled>{subtitle}</PageSubtitleStyled>
+    </PageTitleContainer>
+  );
+};
+
+export default PageTitle;
